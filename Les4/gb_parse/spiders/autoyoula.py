3866137e04daaa300d53c25f7743c69dba00354d
Ubagaly <ubagaly@gmail.com>
2021-01-17 23:48:45 +0300

    Create lessons_4

---
+++ Les4/gb_parse/spiders/autoyoula.py
@@ -0,0 +1,69 @@
+import scrapy
+import re
+import pymongo
+
+
+
+class AutoyoulaSpider(scrapy.Spider):
+    name = 'autoyoula'
+    allowed_domains = ['auto.youla.ru']
+    start_urls = ['https://auto.youla.ru/']
+    css_query = {
+        "brands": "div.TransportMainFilters_block__3etab a.blackLink",
+        "pagination": "div.Paginator_block__2XAPy a.Paginator_button__u1e7D",
+        "ads": "article.SerpSnippet_snippet__3O1t2 a.blackLink",
+    }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.db = pymongo.MongoClient('mongodb://localhost:27017')['gb_parse_21'][self.name]
+
+    def parse(self, response, **kwargs):
+        brands_links = response.css(self.css_query["brands"])
+        yield from self.gen_task(response, brands_links, self.brand_parse)
+
+    def brand_parse(self, response):
+        pagination_links = response.css(self.css_query["pagination"])
+        yield from self.gen_task(response, pagination_links, self.brand_parse)
+        ads_links = response.css(self.css_query["ads"])
+        yield from self.gen_task(response, ads_links, self.ads_parse)
+
+    def ads_parse(self, response):
+        data = {
+            'title': response.css('.AdvertCard_advertTitle__1S1Ak::text').get(),
+            'price': self.get_price(response),
+            'images': [img.attrib.get('src') for img in response.css('figure.PhotoGallery_photo__36e_r img')],
+            'description': response.css('div.AdvertCard_descriptionInner__KnuRi::text').get(),
+            'url': response.url,
+            'autor': self.js_decoder_autor(response),
+            'specification': self.get_specifications(response),
+        }
+
+        print(data)
+        self.db.insert_one(data)
+
+
+
+    def get_specifications(self, response):
+        specifications_result = {itm.css('.AdvertSpecs_label__2JHnS::text').get(): itm.css(
+            '.AdvertSpecs_data__xK2Qx::text').get() or itm.css('a::text').get() for itm in
+                response.css('.AdvertSpecs_row__ljPcX')}
+        return specifications_result
+
+    def get_price(self, response):
+        result = response.css('div.AdvertCard_price__3dDCr::text').get()
+        if result:
+            result = float(result.replace("\u2009", ''))
+            return result
+        else: return None
+
+    def js_decoder_autor(self, response):
+        script = response.css('script:contains("window.transitState = decodeURIComponent")::text').get()
+        re_str = re.compile(r"youlaId%22%2C%22([0-9|a-zA-Z]+)%22%2C%22avatar")
+        result = re.findall(re_str, script)
+        return f'https://youla.ru/user/{result[0]}' if result else None
+
+    @staticmethod
+    def gen_task(response, link_list, callback):
+        for link in link_list:
+            yield response.follow(link.attrib["href"], callback=callback)
